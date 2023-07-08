2190f22955ecc7726c9f357f344ee99c04a6180a
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-07-08 18:50:30 +0300

    changes on hosting deployment

--- bin/deploy_hosting.sh
+++ bin/deploy_hosting.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
+./node_modules/rimraf/dist/cjs/src/bin.js ./static_root/plugin
+# donwload the required release
+curl https://github.com/TheoAnastasiadis/MSXTorrentPlugin/releases/download/v0.0.1-alpha.1/v0.0.1-alpha.1.zip -L --output ./static_root/plugin.zip
+#unzip
+unzip ./static_root/plugin -d ./static_root/plugin
+#cleanup
+./node_modules/rimraf/dist/cjs/src/bin.js ./static_root/plugin.zip
 firebase deploy --only hosting

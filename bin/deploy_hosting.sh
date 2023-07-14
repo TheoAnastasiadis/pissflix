3263f0a591ac24b1a81c1b4f6d88fe86802505b3
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-07-14 15:50:10 +0300

    incremented plugin version

--- bin/deploy_hosting.sh
+++ bin/deploy_hosting.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 ./node_modules/rimraf/dist/cjs/src/bin.js ./static_root/plugin
 # donwload the required release
-curl https://github.com/TheoAnastasiadis/MSXTorrentPlugin/releases/download/v0.1.0-alpha.1/dist.zip -L --output ./static_root/plugin.zip
+curl https://github.com/TheoAnastasiadis/MSXTorrentPlugin/releases/download/v0.1.1-alpha.1/dist.zip -L --output ./static_root/plugin.zip
 #unzip
 unzip ./static_root/plugin -d ./static_root/plugin
 #cleanup
